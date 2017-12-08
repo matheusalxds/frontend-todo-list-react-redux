76618b21846c7167c6badf58498cfadfd6544315
matheus <freeuser153>
2017-12-07 22:45:03 -0200

    App done with react&Redux

--- src/todo/todoActions.js
+++ src/todo/todoActions.js
@@ -9,10 +9,12 @@ export const changeDescription = (e) => ({
 });
 
 export const search = () => {
-  const request = axios.get(`${URL}?sort=-createdAt`);
-  return{
-    type: 'TODO_SEARCHED',
-    payload: request,
+
+  return(dispatch, getState) => {
+    const description = getState().todo.description;
+    const search = description ? `&description__regex=/${description}/` : '';
+    const request = axios.get(`${URL}?sort=-createdAt${search}`)
+      .then(resp => dispatch({type: 'TODO_SEARCHED', payload: resp.data}));
   }
 }
 // func without Thunk
@@ -27,7 +29,7 @@ export const search = () => {
 export const add = (description) => {
   return dispatch => {
     axios.post(URL, {description})
-          .then(resp => dispatch({ type: 'TODO_ADDED', payload: resp.data }))
+          .then(resp => dispatch(clear()))
           .then(resp => dispatch(search()))
   }
 }
@@ -51,4 +53,10 @@ export const remove = (todo) => {
     axios.delete(`${URL}/${todo._id}`)
          .then(resp => dispatch(search()));
   }
+}
+
+export const clear = () => {
+  return [{
+    type: 'TODO_CLEAR'
+  }, search()]
 }
